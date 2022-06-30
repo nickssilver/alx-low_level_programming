34a98ef827516e55d77c59385e4da93db6612b94
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-06-30 18:18:56 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Description:'C program that prints' 
+ * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
 int main(void)
