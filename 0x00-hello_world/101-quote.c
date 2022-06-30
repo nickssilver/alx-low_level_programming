7d48ad1ffabe8bee5e4c2e5a4b571f19927ecf96
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-06-30 18:12:15 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-
+/**
+ * main - Entry point
+ * Description: 'C program that prints' 
+ * Return: Always 0 (Success)
+ */
 int main (void)
 {
-	write(2,  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
