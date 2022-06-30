96572ae2bfec47333407fced3a4f3de4d74a2e19
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-06-30 17:29:00 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 { 
 	printf("Size of a char: %d byte(s)\n",size of(char));
+	printf("Size of an int: %d byte(s)\n",size of(int));
+	printf("Size of a long int: %d byte(s)\n",size of(long int));
+	printf("Size of a long long int: %d byte(s)\n",size of(long long int));
+	printf("Size of a float: %d byte(s)\n",size of(float));
 
 	return (0);
 }
