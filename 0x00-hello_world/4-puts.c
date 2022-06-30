5438c94835b3c219ff9ebb3011bb705a8220d08e
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-06-30 16:49:18 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- *C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
