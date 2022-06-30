f1404ee94f5a481a2219cec12f0f897ffe070e38
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-06-30 16:57:52 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Description: 'C program for print'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
