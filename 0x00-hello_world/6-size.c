fa753388ddfc2956db047b17a63edacccf41538a
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-06-30 17:38:04 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
+{
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
