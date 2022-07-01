d139a8b19455f8ff0cff8fbdfdba22eb05691ec9
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-01 16:42:51 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,18 @@
-fvbbcc
+#include <stdio.h>
+/**
+  * main - entry block
+  * Description: a program that prints the alphabet in lowercase, and then in uppercase
+  * Return: 0
+**/
+int main(void)
+{
+	char c;
+	char C;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (C = 'A'; C <= 'Z'; C++)
+		putchar(C);
+	putchar('\n');
+	return (0);
+}
