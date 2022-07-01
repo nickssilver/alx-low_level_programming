797c4f3f2a6174880594433cca0970501046a0c3
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-01 16:47:07 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,18 @@
-fdhhggfgg
+#include <stdio.h>
+/**
+  * main - entry block
+  * @void: no argument
+  * Return: 0
+**/
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
