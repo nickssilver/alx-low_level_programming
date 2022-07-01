d55c36c56f28c1d8985278cff6c55a5843d4fa1c
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-01 21:20:59 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,54 @@
-dfvdvddfd
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int digit1;
+	int digit2;
+	int digit3;
+
+	digit1 = 48;
+	digit2 = 49;
+	digit3 = 50;
+	while  ((digit1 < 56) && (digit2 < 57) && (digit3 < 58))
+	{
+		putchar(digit1);
+		putchar(digit2);
+		putchar(digit3);
+		if ((digit1 == 55) && (digit2 == 56) && (digit3 == 57))
+		{
+			putchar('\n');
+			digit1++;
+			digit2++;
+			digit3++;
+		}
+		else
+		{
+			putchar(44);
+			putchar(32);
+			if (digit3 < 57)
+			{
+				digit3++;
+			}
+			else if ((digit2 < 56) && (digit3 == 57))
+			{
+				digit3 = 1 + ++digit2;
+			}
+			else
+			{
+				digit2 = 1 + ++digit1;
+				digit3 = 1 + digit2;
+			}
+		}
+	}
+	return (0);
+}
+
