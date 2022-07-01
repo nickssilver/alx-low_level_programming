4b8831c3754daa73582feaf394eaabdc1dc6a5bf
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-01 16:33:32 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+/**
+*main - Printthe alphabetic
+* a program that prints the alphabet in lowercase, followed by a new line.
+*Return: Always (success)
+*/
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
