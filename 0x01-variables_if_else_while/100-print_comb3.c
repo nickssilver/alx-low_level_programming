05e5d0af944e59659d82714611958ca73858c436
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-01 20:43:41 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,33 @@
-dfdfvvv
+#include <stdio.h>
+/**
+* main - loop through variables a and b printing digits
+* description: prints all possible different combinations of two digits
+* Return: print to stdout all possible different combinations of twodigits
+**/
+int main(void)
+{
+int a = '0';
+int b;
+
+while (a <= '9')
+{
+b = a + 1;
+while (b <= '9')
+{
+putchar(a);
+putchar(b);
+if (a == '8' && b == '9')
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+b++;
+}
+a++;
+}
+return (0);
+}
