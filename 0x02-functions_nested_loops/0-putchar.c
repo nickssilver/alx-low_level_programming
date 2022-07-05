853a87ef9672146ce8fe99115a38d3762c3c8290
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-05 22:13:16 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,20 @@
-ssffffff
+#include "main.h"
+
+/**
+ * main - Entry Block
+  *Description: prints _putchar followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char d[8] = "_putchar";
+
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(d[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
