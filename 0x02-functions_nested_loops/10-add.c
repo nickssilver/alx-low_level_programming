3e301aa4543e9f685c749de0b7cbdfb45619b1d6
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-05 23:41:09 +0300

    Completed Task

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,15 @@
-dfes
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
