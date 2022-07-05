036b16aacb97d5748faa7d36b551a83fd385d4e3
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-05 23:29:47 +0300

    Completed Task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,25 @@
-sddfvddf
+#include "main.h"
+/**
+  * print_sign - Prints the sign of a number
+  * @n: The number to be checked
+  *
+  * Return: 1 for positive num, -1 for negative num and 0 for zero.
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
