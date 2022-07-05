36763fdaf56839fd4ab8b2d10fb3736d1e294fea
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-05 23:18:18 +0300

    Completed Task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,14 @@
-fdfdf
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase letters
+ * @c: character to check
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
