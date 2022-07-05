3fb6bbefb351e40523e3f19275d291ec976e2270
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-05 22:47:46 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,100 +1,15 @@
-<<<<<<< HEAD
-lude "main.h"
-
-/**
- *
- *  * main - Entry Block
- *
- *   * Description: prints _putchar followed by a new line
- *
- *    * Return: 0
- *
- *     */
-
-=======
 #include "main.h"
 /**
  * main - Entry Block
- * Description: prints _putchar followed by a new line
+  *Description: prints _putchar followed by a new line
  * Return: 0
  */
->>>>>>> a59bc19173b6fb3fa2dbb6fa5d2ca1310ea876b8
 int main(void)
-
 {
-<<<<<<< HEAD
-
-		char c[8] = "_putchar";
-
-			int i = 0;
-
-				while (i < 8)
-
-						{
-
-									_putchar(c[i]);
-
-											i++;
-
-												}
-
-					_putchar('\n');
-
-						return (0);#include "main.h"
-
-							/**
-							 *
-							 *  * main - Entry Block
-							 *
-							 *   * Description: prints _putchar followed by a new line
-							 *
-							 *    * Return: 0
-							 *
-							 *     */
-
-							int main(void)
-
-							{
-
-									char c[8] = "_putchar";
-
-										int i = 0;
-
-											while (i < 8)
-
-													{
-
-																_putchar(c[i]);
-
-																		i++;
-
-																			}
-
-												_putchar('\n');
-
-													return (0);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
 	char c[8] = "_putchar";
+
 	int i = 0;
+
 	while (i < 8)
 	{
 		_putchar(c[i]);
@@ -102,4 +17,4 @@ int main(void)
 	}
 	_putchar('\n');
 	return (0);
->>>>>>> a59bc19173b6fb3fa2dbb6fa5d2ca1310ea876b8
+}
