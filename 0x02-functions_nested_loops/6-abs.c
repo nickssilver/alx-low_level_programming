d2e7db8a6df87ab02152fc07d62610e95822d563
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-05 23:31:28 +0300

    Completed Task

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,20 @@
-dddds
+#include "main.h"
+/**
+ * _abs - outputs absolut value of integer input
+ * @n: integer to check
+ *
+ * Return: n
+ */
+
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+
+	else
+	{
+		n *= -1;
+		return (n);
+	}
+}
