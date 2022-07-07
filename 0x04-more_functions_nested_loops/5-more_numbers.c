989dcad9ae7499125a9bf16fd62e8079653b1484
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-07 16:50:22 +0300

    Completed

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14; 10 times.
+ *
+ * Return: void.
+ */
+
+void more_numbers(void)
+{
+	int num, count;
+
+	for (count = 0; count <= 9; count++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
