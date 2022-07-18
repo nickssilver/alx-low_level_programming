cb26d1a025741a6274f4485616c879fd692cd5e3
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-18 17:16:14 +0300

    Task Done

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,6 @@
 #include "main.h"
-5-strstr.c 7-print_chessboard.c 8-print_diagsums.c 100-set_string.c 101-crackme_password
-
-**
-5-strstr.c 7-print_chessboard.c 8-print_diagsums.c 100-set_string.c 101-crackme_password
-
-*_memset -   fills memory with a constant byte
+/**
+ *_memset -   fills memory with a constant byte
  *@s: pointer block of memory to fill
  *@b: constant byte
  *@n: bytes of the memory
