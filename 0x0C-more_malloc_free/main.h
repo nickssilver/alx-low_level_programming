19bd771dcecf6827e3ade1c2305dc5ee6ac32987
Nicks <51230602+nickssilver@users.noreply.github.com>
2022-07-28 21:05:51 +0300

    Task Done

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef FILE_MAIN
+#define FINE_MAIN
+
+void *malloc_checked(unsigned int b);
+int _putchar(char c);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
