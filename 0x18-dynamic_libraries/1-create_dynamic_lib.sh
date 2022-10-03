e57cfa96d76c43152c80cbee106c49d0475df588
nickssilver <51230602+nickssilver@users.noreply.github.com>
2022-10-03 14:26:00 -0700

    error on dir

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gcc *.c -c -fPIC
-gcc *.o -shared -o liball.so
--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P /tmp https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
